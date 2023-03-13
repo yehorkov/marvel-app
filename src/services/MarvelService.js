db448d99528318c4dba11fcfe4bb20e2a377dac3
Егор Коваль <yehor_kov@MacBook-Air-Egor.local>
2023-03-13 22:46:56 +0200

    Add single comics page

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,6 +4,7 @@ const useMarvelService = () => {
     const {loading, request, error, clearError} = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    // const _apiKey = "apikey=c5d6fc8b83116d92ed468ce36bac6c62";
     const _apiKey = 'apikey=3d7c5d15c6b68f1c5b93883dde8524ed';
     const _baseOffset = 210;
 
